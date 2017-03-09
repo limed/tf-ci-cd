7839b87d95522c209ddf4cb5ddf7952bcdd23529
limed <limed@mozilla.com>
2017-03-09 14:27:28 -0800

    Return codepipeline and codedeploy role id

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
+output codebuild_role_id {
+  value = "${aws_iam_role.codepipeline_role.id}"
+}
+
+output codepipeline_role_id {
+  value = "${aws_iam_role.codepipeline-role.id}"
+}
+
 output codebuild_role_arn {
   value = "${aws_iam_role.codebuild_role.arn}"
 }
