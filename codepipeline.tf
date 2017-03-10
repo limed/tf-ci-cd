36df3efe2ee201d45b1c123de867748e5f8b2699
limed <limed@mozilla.com>
2017-03-10 11:44:15 -0800

    Fixing some formatting

--- codepipeline.tf
+++ codepipeline.tf
@@ -1,22 +1,22 @@
 resource aws_s3_bucket "artifact-store" {
-	bucket	= "${var.instance_name}-artifact-${random_id.uuid.hex}"
-	acl			= "private"
+  bucket  = "${var.instance_name}-artifact-${random_id.uuid.hex}"
+  acl     = "private"
 }
 
 resource "aws_iam_role" "codepipeline-role" {
-	name	= "${var.instance_name}-codepipeline-role"
-	assume_role_policy = <<EOF
+  name	= "${var.instance_name}-codepipeline-role"
+  assume_role_policy = <<EOF
 {
-	"Version": "2012-10-17",
-	"Statement": [
-		{
-			"Effect": "Allow",
-			"Principal": {
-				"Service": "codepipeline.amazonaws.com"
-			},
-			"Action": "sts:AssumeRole"
-		}
-	]
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codepipeline.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
 }
 EOF
 }
@@ -32,12 +32,12 @@ resource aws_iam_role_policy "codepipeline-policy" {
     {
       "Effect": "Allow",
       "Action": [
-				"s3:PutObject",
-				"s3:GetObject",
+        "s3:PutObject",
+        "s3:GetObject",
         "s3:GetObjectVersion",
         "s3:GetBucketVersioning"
       ],
-			"Resource": [
+      "Resource": [
         "${aws_s3_bucket.artifact-store.arn}",
         "${aws_s3_bucket.artifact-store.arn}/*"
       ]
@@ -56,35 +56,35 @@ EOF
 }
 
 resource aws_codepipeline "codepipeline" {
-	name			= "${var.instance_name}"
-	role_arn  = "${aws_iam_role.codepipeline-role.arn}"
+  name      = "${var.instance_name}"
+  role_arn  = "${aws_iam_role.codepipeline-role.arn}"
 
-	artifact_store {
-  	location 	= "${aws_s3_bucket.artifact-store.bucket}"
-		type			= "S3"
-	}
-
-	stage {
-  	name	= "Source"
-		action {
-    	name			        = "Source"
-			category	        = "Source"
-			owner			        = "ThirdParty"
-			provider	        = "GitHub"
-			version		        = "1"
-      output_artifacts  = [ "${var.instance_name}" ]
-
-			configuration {
-      	Owner		    = "${var.github_user}"
-				Repo		    = "${var.github_repo}"
-				Branch	    = "${var.github_branch}"
-        OAuthToken  = "${var.github_token}"
-			}
-		}
-	}
+  artifact_store {
+    location  = "${aws_s3_bucket.artifact-store.bucket}"
+    type      = "S3"
+  }
 
   stage {
-  	name = "Build"
+    name  = "Source"
+    action {
+      name              = "Source"
+      category          = "Source"
+      owner             = "ThirdParty"
+      provider          = "GitHub"
+      version           = "1"
+      output_artifacts  = [ "${var.instance_name}" ]
+
+      configuration {
+        Owner       = "${var.github_user}"
+        Repo        = "${var.github_repo}"
+        Branch      = "${var.github_branch}"
+        OAuthToken  = "${var.github_token}"
+      }
+    }
+  }
+
+  stage {
+    name = "Build"
 
     action {
       name              = "Build"
@@ -98,6 +98,6 @@ resource aws_codepipeline "codepipeline" {
       configuration {
         ProjectName = "${aws_codebuild_project.project.name}"
       }
-		}
-	}
+    }
+  }
 }
